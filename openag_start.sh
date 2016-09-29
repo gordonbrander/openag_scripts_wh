492fa6c2edc2cc130432a057b2bbe5d2d2b6dd57
Gordon Brander <gordonbrander@users.noreply.github.com>
2016-09-29 15:18:41 -0700

    Configure fixtures before running

--- openag_start.sh
+++ openag_start.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 rosrun openag_brain load_fixture default
 rosrun openag_brain load_fixture wh
-rosrun openag_brain load_fixture remove_arduino
 rosrun openag_brain main &
-rosrun rosserial_python serial_node.py /dev/ttyACM0 &
